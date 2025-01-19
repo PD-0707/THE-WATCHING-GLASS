205bc6a9a43f836f3e1a8bdf51334ce8d6cd6772
Priyadarshini <priyadarshini0727@gmail.com>
2025-01-19 21:54:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProgressProvider } from './contexts/ProgressContext'; // Import the provider
 import HomePage from './components/HomePage';
